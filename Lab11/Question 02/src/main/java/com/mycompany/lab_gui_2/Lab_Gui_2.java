5ed50a9a7a938dc591e6dae0cce2c5ca229e7dd7
upekshaip <upekshaip@gmail.com>
2023-07-20 16:09:44 +0530

    Lab11 added

---
+++ Lab11/Question 02/src/main/java/com/mycompany/lab_gui_2/Lab_Gui_2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_gui_2;
+
+/**
+ *
+ * @author d4rkg
+ */
+public class Lab_Gui_2 {
+
+    public static void main(String[] args) {
+        gui g = new gui();
+        g.show();
+    }
+}
---
+++ Lab11/Question 03/src/main/java/com/mycompany/lab_gui_3/Lab_GUI_3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab_gui_3;
+
+/**
+ *
+ * @author d4rkg
+ */
+public class Lab_GUI_3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
